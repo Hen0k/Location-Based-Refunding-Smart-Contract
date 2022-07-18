35c0f3d80b7c7d899e4e39dc5f6a9aacd8146ff6
Hen0k <henoktilaye7@gmail.com>
2022-07-18 10:21:28 +0300

    admin: add employee adder and viewer

--- admin-portal/src/components/employeeForm.js
+++ admin-portal/src/components/employeeForm.js
@@ -1,58 +1,84 @@
+import "./employeeForm.css"
 import { ethers, Contract } from 'ethers';
-import { useState, useEffect } from "react";
-import HelloWorld from '../HelloWorld.json';
+import { useState } from "react";
+import LBRC from './LBRC.json';
 
-
-let url = 'http://127.0.0.1:7545';
-let customHttpProvider = new ethers.providers.JsonRpcProvider(url);
-const contractAddress = "0x6a9338F765497Faa20b12b91b6DE24fcE99Bf762"
-let contract = new Contract(contractAddress, HelloWorld.abi, customHttpProvider)
+const abi = LBRC.abi;
 
 
 
 const EmployeeForm = () => {
 
-    let [blockNum, getBlockNum] = useState(0)
-    let [message, setMessage] = useState("")
-    const loadMessage = async () => {
-      const message = await contract.getMessage()
-      setMessage(message)
-    }
-    
-    
-    useEffect(() => {
-      loadMessage()
-      return () => {}
-    }, [])
-  
-    const clickHandler = () => {
-      customHttpProvider.getBlockNumber().then((result) => {
-        console.log("Current block number: " + result);
-        getBlockNum(result)
-      });
-    }
-  
-    const changeMessage = async (event) => {
-      event.preventDefault();
-      const message = event.target.msg.value
-      const signer = customHttpProvider.getSigner()
-      let c = new Contract(contractAddress, HelloWorld.abi, signer)
-      await c.setMessage(message)
-      loadMessage()
-  
-    }
+  let [empData, setEmployeeData] = useState("")
+  // let [message, setMessage] = useState("")
 
-    return <>
-        <button onClick={clickHandler}>
-            getBlockNumber
-        </button>
-        <p>Block Number: {blockNum}</p>
-        <p>Message: {message}</p>
-        <form onSubmit={changeMessage}>
-            <input type="text" name="msg" placeholder="message"></input>
-            <input type="submit" value="ok" />
-        </form>
-    </>
+  const createAdminSignedContract = () => {
+    const contractAddress = "0x13F68498cC89d195C12741c426348B65b74832Ff"
+    const privateKey = "a8c80296d57ea1606736a7cf5d3e07f73f9aef581c049c313e6ba53a684f660c";
+    const url = "http://127.0.0.1:7545";
+    const provider = new ethers.providers.JsonRpcProvider(url);
+    const signer = new ethers.Wallet(
+      privateKey,
+      provider
+    );
+    const contract = new Contract(contractAddress, abi, signer);
+    return contract
+  }
+
+  const clickHandler = async (event) => {
+    event.preventDefault();
+    console.log("Clicked")
+    const addr = event.target.addr.value;
+    const contract = createAdminSignedContract()
+    const empData = await contract.empContractStatus(addr);
+    setEmployeeData(empData);
+    // console.log(addr);
+    // setEmployeeData(empData);
+
+  }
+
+  const changeStatus = async (event) => {
+    event.preventDefault();
+    const addr = event.target.addr.value
+    const lat = event.target.lat.value
+    const lon = event.target.lon.value
+    const radius = event.target.radius.value
+    const payAmount = event.target.payAmount.value
+    const reqCount = event.target.reqCount.value
+
+    const contract = createAdminSignedContract()
+
+    const status = await contract.setAccount(
+      addr,
+      lat,
+      lon,
+      radius,
+      payAmount,
+      reqCount)
+    console.log(status)
+  }
+
+  return <>
+    <form onSubmit={clickHandler}>
+      <input type="text" name="addr" placeholder="public address"></input>
+      <input type="submit" value="get employee data" />
+    </form>
+    <p>Latitude: {empData[0]}</p>
+    <p>Longitude: {empData[1]}</p>
+    <p>Radius: {empData[2]}</p>
+    <p>Payment Amount: {empData[3]}</p>
+    <p>Check-in count: {empData[4]}</p>
+    <p>Required check-ins: {empData[5]}</p>
+    <form onSubmit={changeStatus}>
+      <input type="text" name="addr" placeholder="public address"></input>
+      <input type="number" min="-90" max="90" name="lat" placeholder="Latitude"></input>
+      <input type="number" min="-180" max="180" name="lon" placeholder="Longitude"></input>
+      <input type="number" min="0" name="radius" placeholder="radius"></input>
+      <input type="number" min="0" name="payAmount" placeholder="eth amount"></input>
+      <input type="number" min="1" name="reqCount" placeholder="minimum successfully check in"></input>
+      <input type="submit" value="ok" />
+    </form>
+  </>
 }
 
 export default EmployeeForm;
